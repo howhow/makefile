4ed04102fa8e8a9e9309a8cca4eb6a6baff3ec23
How Chen <how.chen@gemalto.com>
2017-05-09 15:04:21 +0800

    add check option

--- sys-make/rules.mk
+++ sys-make/rules.mk
@@ -1,26 +1,30 @@
 # rules makefile
 #
-#    @File:      rules.mk
-#    @Author:    How.Chen
-#    @Version:   1.0
-#    @Date:      27h/April/2017
-#    @Note:
-#                -V1.0
-#                - init commit
-#                - add rule for obj
-#                - add rule for depend
-#                - add rule for lib
-#
+#    @File:     rules.mk
+#    @Author:   How.Chen
+#    @Version:  2.0
+#    @Date:		9h/May/2017
+#    @Note:	
+#    			- V2.0
+#    			- add comments
+#				- add debug print
+#				
 
 # define general path and output for each sub module
 OUTS = $(OUTPUT_DIR)/$(MODULE)
 OBJS = $(addprefix $(OUTS)/,$(SRC_FILES:.c=.o))
 DEPS = $(addprefix $(OUTS)/,$(SRC_FILES:.c=.d))
 
-# workaround for link
+CC_DEFINE = $(addprefix -D, $(CC_DEFS))
+CC_INC_PATH = $(addprefix -I , $(INC_PATH))
+
+
+TARGET_NAME = test_makefile
+
+# workaround for link, need all objs at once
 FULL_O=$(shell find $(OUTS) -name "*.o")
 
-.PHONY: link clean
+.PHONY: link clean verbose
 
 # generate lib file
 $(OUTS)/$(MOD_LIB): $(OBJS)
@@ -28,20 +32,32 @@ $(OUTS)/$(MOD_LIB): $(OBJS)
 	@echo "ar $(notdir $@)"
 
 # compile from source to objs include for dependence
+# $< means each %.c, $@ means each $(OUTS)/%.o
 $(OUTS)/%.o: %.c
 	@mkdir -p $(OUTS)
-#	@$(CC) $(CFLAG) $(CC_DEFS) -I$(INC_PATH) -c $<
-#	@$(CC) $(CFLAG) $(CC_DEFS) -I$(INC_PATH) -MM $< > $*.d 
-#	@$(CC) $(CFLAG) $(CC_DEFS) -I$(INC_PATH) -MM -MF  $(patsubst %.o,%.d,$@) -o $@ $< 
-#	@$(CC) $(CFLAG) $(CC_DEFS) -I$(INC_PATH) -MMD -MF $(patsubst %.o,%.d,$@) -MT $@ -o $@ $< 
-	@$(CC) $(CFLAG) $(CC_DEFS) -I$(INC_PATH) -MMD -c $< -o $@
+	@$(CC) $(CFLAG) $(CC_DEFINE) $(CC_INC_PATH) -MMD -c $< -o $@
 	@echo "cc $(notdir $<)"
 
 # workaround for link
+# $(CFLAG) does not exported
 link:
-	@echo "Link executable"
-#	@echo $(FULL_O)
-	@gcc -ggdb -Wall -fprofile-arcs -ftest-coverage $(FULL_O) -Wl,-Map=$(OUTPUT_DIR)/test.map -o $(OUTPUT_DIR)/test
+	@echo "Link executable: $(OUTPUT_DIR)/$(TARGET_NAME)"
+	@gcc -ggdb -Wall -fprofile-arcs -ftest-coverage $(FULL_O) -Wl,-Map=$(OUTPUT_DIR)/$(TARGET_NAME).map -o $(OUTPUT_DIR)/$(TARGET_NAME)
+
+verbose:
+	@echo "mod: $(MODULE)"
+	@echo "libs: $(MOD_LIB)"
+	@echo "src_path: $(SRC_PATH)"
+	@echo "inc_path: $(INC_PATH)"
+	@echo "srcs: $(SRC_FILES)"
+	@echo "outs: $(OUTS)"
+	@echo "objs: $(OBJS)"
+	@echo "dpes: $(DEPS)"
+	@echo "flag: $(CFLAG)"
+	@echo "targ: $@"
+	@echo "enabled features: $(FEATURE)"
+	@echo "cc defines: $(CC_DEFINE)"
+	@echo "cc include: $(CC_INC_PATH)"
 
 # remove output for each sub module
 clean:
@@ -50,3 +66,10 @@ clean:
 # include dependence if has
 -include $(DEPS)
 
+########## HISTORY ##########
+# -V1.0
+# - init commit
+# - add rule for obj
+# - add rule for depend
+# - add rule for lib
+#############################
